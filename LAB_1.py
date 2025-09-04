f7638fb0e1de0654ff03f1a2a55d874a86c8b9a2
LissKovzogo <Silika0Clea@yandex.ru>
2025-09-04 09:32:45 +0300

    Commit 7: исправление ошибки с перводом строки в целочисленный тип данных

--- LAB_1.py
+++ LAB_1.py
@@ -1,6 +1,6 @@
 #! Вычисление факториала
 print("Поиск факториала числа: ")
-number = input("Введите число: ")
+number = int(input("Введите число: "))
 result = 1
 for i in range(1,number+1):
     result *= i
